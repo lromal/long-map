bd8672f23383e1788c547eeb17c7c53589ba3c28
roma <roma@example.com>
2020-04-22 15:40:13 +0300

    3697 Finish development of LongMapImpl class

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,43 +1,287 @@
 package de.comparus.opensource.longmap;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class LongMapImpl<V> implements LongMap<V> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_DENSITY = 0.75f;
+
+    private LinkedList<Entry<V>>[] buckets;
+    private int size = 0;
+    private int maxSize;
+    private int capacity;
+    private double density;
+
+
+    /**
+     * @param capacity the initial size of a hash table array. It should be >= 0.
+     * @param density the load factor of a hash table. It should be >= 0.
+     */
+    public LongMapImpl(int capacity, float density) {
+
+        if(capacity <= 0) {
+            throw new IllegalArgumentException("Illegal capacity: " + capacity);
+        }
+
+        if(density <= 0) {
+            throw new IllegalArgumentException("Illegal density: " + density);
+        }
+
+        this.capacity = capacity;
+
+        this.density = density;
+
+        calculateMaxSize();
+
+        buckets = new LinkedList[this.capacity];
+    }
+
+    /**
+     * @param capacity the initial size of a hash table array. It should be >= 0.
+     */
+    public LongMapImpl(int capacity) {
+
+        this(capacity, DEFAULT_DENSITY);
+    }
+
+    public LongMapImpl() {
+
+        this(DEFAULT_CAPACITY, DEFAULT_DENSITY);
+    }
+
     public V put(long key, V value) {
-        return null;
+
+        Entry<V> entry = new Entry<>(key, value);
+
+        resize();
+
+        return putEntry(entry);
     }
 
+    private void resize() {
+
+        if(size < maxSize) {
+            return;
+        }
+
+        capacity *= 2;
+
+        calculateMaxSize();
+
+        List<Entry<V>> list = convertToStream().collect(Collectors.toList());
+
+        clear();
+
+        buckets = new LinkedList[this.capacity];
+
+        for (Entry<V> entry : list) {
+            put(entry.key, entry.getValue());
+        }
+
+    }
+
+
+    @Override
     public V get(long key) {
-        return null;
+
+        Entry<V> searchedEntry = new Entry<>(key, null);
+
+        LinkedList<Entry<V>> list = getBucket(searchedEntry, false);
+
+        int index = list.indexOf(searchedEntry);
+
+        if(index < 0) {
+            return null;
+        }
+
+        return list.get(index).getValue();
     }
 
+    @Override
     public V remove(long key) {
-        return null;
+
+        Entry<V> searchedEntry = new Entry<>(key, null);
+
+        LinkedList<Entry<V>> list = getBucket(searchedEntry, false);
+
+        int index = list.indexOf(searchedEntry);
+
+        if(index < 0) {
+            return null;
+        }
+
+        searchedEntry = list.get(index);
+
+        list.remove(index);
+
+        size--;
+
+        return searchedEntry.getValue();
     }
 
+    @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
+    @Override
     public boolean containsKey(long key) {
-        return false;
+
+        Entry<V> searchedEntry = new Entry<>(key, null);
+
+        LinkedList<Entry<V>> list = getBucket(searchedEntry, false);
+
+        int index = list.indexOf(searchedEntry);
+
+        return index >= 0;
     }
 
     public boolean containsValue(V value) {
-        return false;
+
+        Optional<Entry<V>> searchedEntry = convertToStream().parallel().filter(a -> a.value == value).findAny();
+
+        return searchedEntry.isPresent();
     }
 
+    @Override
     public long[] keys() {
-        return null;
+
+        if(size == 0) {
+            return null;
+        }
+
+        return convertToStream().mapToLong((a) -> a.key).toArray();
     }
 
+    @Override
     public V[] values() {
-        return null;
+
+        if(size == 0) {
+            return null;
+        }
+
+        List<Object> objects = convertToStream().map((a) -> a.value).collect(Collectors.toList());
+
+        return objects.toArray((V[]) Array.newInstance(objects.get(0).getClass(), objects.size()));
     }
 
+    @Override
     public long size() {
-        return 0;
+        return size;
     }
 
+    @Override
     public void clear() {
 
+        for(int i = 0; i < buckets.length; i++) {
+
+            if(buckets[i] == null) {
+                continue;
+            }
+
+            buckets[i].clear();
+            buckets[i] = null;
+        }
+
+        size = 0;
+
+    }
+
+    private void calculateMaxSize() {
+
+        maxSize = Math.toIntExact(Math.round(capacity * density));
+
+    }
+
+    private Stream<Entry<V>> convertToStream() {
+        return Arrays.stream(buckets).filter(Objects::nonNull).flatMap(Collection::stream);
+    }
+
+    private V putEntry(Entry<V> entry) {
+
+        LinkedList<Entry<V>> bucket = getBucket(entry, true);
+
+        int index = bucket.indexOf(entry);
+
+        if(index >= 0) {
+
+            return bucket.get(index).setValue(entry.getValue());
+
+        }
+
+        bucket.add(entry);
+
+        size++;
+
+        return null;
+
+    }
+
+    private LinkedList<Entry<V>> getBucket(Entry entry, boolean createBucket) {
+
+        int hash = entry.hashCode();
+
+        LinkedList<Entry<V>> list = buckets[hash];
+
+        if(list == null) {
+            list = new LinkedList<>();
+
+            if(createBucket) {
+                buckets[hash] = list;
+            }
+        }
+
+        return list;
+    }
+
+
+    class Entry<T> {
+
+        private long key;
+        private T value;
+
+        Entry(long key, T value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public boolean equals(Object object) {
+
+            if (object == this) {
+                return true;
+            }
+
+            return object instanceof Entry && equalsEntry(object);
+
+        }
+
+        private boolean equalsEntry(Object object) {
+
+            Entry entry = (Entry) object;
+
+            return entry.key == this.key;
+
+        }
+
+        T getValue() {
+            return value;
+        }
+
+        public int hashCode() {
+            return Long.hashCode(key) % capacity;
+        }
+
+        T setValue(T value) {
+
+            T oldValue = this.value;
+
+            this.value = value;
+
+            return  oldValue;
+        }
     }
 }
---
+++ src/test/java/de/comparus/opensource/longmap/LongMapTest.java
@@ -0,0 +1,228 @@
+package de.comparus.opensource.longmap;
+
+import org.junit.*;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class LongMapTest {
+
+    private static long[] keys;
+    private static String[] values;
+    private LongMap<String> testData;
+    private LongMap<String> testEmptyData;
+    private final static int TEST_DATA_LENGTH = 4;
+
+
+
+    @BeforeClass
+    public static void init() {
+        keys = new long[TEST_DATA_LENGTH];
+        keys[0] = 1;
+        keys[1] = 2;
+        keys[2] = 3;
+        keys[3] = 4;
+
+        values = new String[TEST_DATA_LENGTH];
+        values[0] = "test1";
+        values[1] = "test2";
+        values[2] = "test3";
+        values[3] = "test4";
+    }
+
+    @Before
+    public void createTestData() {
+
+        testData = new LongMapImpl<>(3);
+
+        for(int i = 0; i < keys.length; i++) {
+            testData.put(keys[i], values[i]);
+        }
+
+        testEmptyData = new LongMapImpl<>();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructor_IllegalCapacity_ExceptionThrown() {
+
+        testData = new LongMapImpl<>(-10);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructor_IllegalDensity_ExceptionThrown() {
+
+        testData = new LongMapImpl<>(16, -10);
+
+    }
+
+
+    @Test
+    public void size_InitialTestData_TestDataLength() {
+
+        assertEquals("Test data has the wrong size", TEST_DATA_LENGTH, testData.size());
+
+    }
+
+    @Test
+    public void put_AddedNewEntryWithNotExistingKey_SizeIsSameAndOldValueIsNotNull() {
+
+        String oldValue = testData.put(4, "test44");
+
+        assertEquals("Test data has the wrong size", TEST_DATA_LENGTH, testData.size());
+
+        assertEquals("Old value is wrong", "test4", oldValue);
+
+    }
+
+    @Test
+    public void put_AddedNewEntryWithNotExistingKey_SizeIncreasedAndOldValueIsNull() {
+
+        String oldValue = testData.put(5, "test5");
+
+        assertEquals("Test data has the wrong size", TEST_DATA_LENGTH + 1, testData.size());
+
+        assertNull("Old value is wrong", oldValue);
+
+    }
+
+    @Test
+    public void get_EntryExists_test4() {
+
+        String value = testData.get(4L);
+
+        assertEquals("Founded value is wrong", "test4", value);
+
+    }
+
+    @Test
+    public void get_EntryNotExists_Null() {
+
+        String value = testData.get(5L);
+
+        assertNull("Some value was found by the wrong key", value);
+
+    }
+
+    @Test
+    public void containsKey_EntryExists_True() {
+
+        assertEquals("Key was not found", true, testData.containsKey(4L));
+
+    }
+
+    @Test
+    public void containsKey_EntryNotExists_False() {
+
+        assertEquals("Some value was found by the wrong key", false, testData.containsKey(5L));
+
+    }
+
+    @Test
+    public void isEmpty_InitialTestData_False() {
+
+        assertEquals("Test data is empty", false, testData.isEmpty());
+
+    }
+
+    @Test
+    public void isEmpty_InitialEmptyTestData_True() {
+
+        assertEquals("Empty data contains some values", true, testEmptyData.isEmpty());
+
+    }
+
+    @Test
+    public void remove_OneEntryRemoved_TestDataSizeDecreasedAndOldValueIsNotNull() {
+
+        String value = testData.remove(1L);
+
+        assertEquals("Test data has the wrong size", TEST_DATA_LENGTH - 1, testData.size());
+
+        assertEquals("Old value is wrong", "test1", value);
+
+    }
+
+    @Test
+    public void remove_KeyNotExists_TestDataSizeNotDecreasedAndOldValueIsNull() {
+
+        String value = testData.remove(5L);
+
+        assertEquals("Test data has the wrong size", TEST_DATA_LENGTH, testData.size());
+
+        assertNull("Old value is not null", value);
+
+    }
+
+    @Test
+    public void clear_EmptyMap_SizeIs0() {
+
+        testData.clear();
+
+        assertEquals("Test data is not empty", 0, testData.size());
+
+    }
+
+    @Test
+    public void containsValue_ValueExists_True() {
+
+        assertEquals("Existed value was not found", true, testData.containsValue("test1"));
+
+    }
+
+    @Test
+    public void containsValue_ValueNotExists_False() {
+
+        assertEquals("Some entry was found by the wrong value", false, testData.containsValue("test5"));
+
+    }
+
+    @Test
+    public void containsValue_SearchingInEmptyMap_False() {
+
+        assertEquals("Some entry was found in the empty map", false, testEmptyData.containsValue("test1"));
+
+    }
+
+    @Test
+    public void keys_InitialTestData_TestDataKeys() {
+
+        long[] testDataKeys = testData.keys();
+
+        Arrays.sort(testDataKeys);
+
+        assertArrayEquals("Some keys are missing", keys, testDataKeys);
+
+    }
+
+    @Test
+    public void keys_InitialTestEmptyData_Null() {
+
+        long[] testDataKeys = testEmptyData.keys();
+
+        assertNull("Some keys were found in the empty map", testDataKeys);
+
+    }
+
+    @Test
+    public void values_InitialTestData_TestDataValues() {
+
+        String[] testDataValues = testData.values();
+
+        Arrays.sort(testDataValues);
+
+        assertArrayEquals("Some values are missing", values, testDataValues);
+
+    }
+
+    @Test
+    public void values_InitialTestEmptyData_Null() {
+
+        String[] testDataValues = testEmptyData.values();
+
+        assertNull("Some values were found in the empty map", testDataValues);
+
+    }
+
+}
